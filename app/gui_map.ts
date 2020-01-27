6c69a7ecb68ecc5d8af8e388c73d541152c9d612
adammmertel <mertel.adam@gmail.com>
2020-01-27 16:47:58 +0100

    gui map drawing map

--- app/gui_map.ts
+++ app/gui_map.ts
@@ -1,7 +1,5 @@
 import * as d3 from 'd3';
-require('d3-geo');
-
-console.log(d3);
+import * as d3tile from 'd3-tile';
 
 async function loadNames() {
 	return await d3.tsv(require('./data/gui/names.tsv'));
@@ -23,7 +21,7 @@ loadNames().then((persons) => {
 			/*
 			 geocode persons
 			*/
-			const personWithPlace = persons.filter((person: any) => {
+			const personWithPlace = persons.filter((p) => p.dissident_minister === 'no').filter((person: any) => {
 				// assigning place name to person
 				const both = person.origin_or_residence;
 				const origin = person.origin;
@@ -97,6 +95,8 @@ loadNames().then((persons) => {
 				}
 			});
 
+			console.log(personWithPlace);
+
 			/* 
 				summing edges for groups
 			*/
@@ -105,11 +105,13 @@ loadNames().then((persons) => {
 				group.edges = {};
 				group.persons.forEach((person) => {
 					person.edges.forEach((personEdge) => {
-						const targetPlace = personEdge.to.place.name;
-						if (targetPlace in group.edges) {
-							group.edges[targetPlace].push(person.id);
-						} else {
-							group.edges[targetPlace] = [ person.id ];
+						if (personEdge.type === 'source') {
+							const targetPlace = personEdge.to.place.name;
+							if (targetPlace in group.edges) {
+								group.edges[targetPlace].push(person.id);
+							} else {
+								group.edges[targetPlace] = [ person.id ];
+							}
 						}
 					});
 				});
@@ -122,21 +124,86 @@ loadNames().then((persons) => {
 			const width = 1500;
 			const height = 800;
 
-			var projection = d3.geoMercator().scale(12000).center([ 0, 44 ]);
+			const tileSize = 256;
+			var projection = d3.geoMercator().scale(20000).center([ 1.18, 43.8 ]);
 
 			const svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
 			var path = d3.geoPath().projection(projection);
 
+			const url = (x, y, z) => `https://stamen-tiles-a.a.ssl.fastly.net/terrain-background/${z}/${x}/${y}.png`;
+			const tiler = d3tile
+				.tile()
+				.size([ width, height ])
+				.scale(projection.scale() * 2 * Math.PI)
+				.tileSize(tileSize)
+				.translate(projection([ 0, 0 ]));
+
+			const tiles = tiler();
+			const [ tx, ty ] = tiles.translate;
+			const k = tiles.scale;
+
+			tiles.map(([ x, y, z ]) => {
+				svg
+					.append('image')
+					.datum(function(d) {
+						return d;
+					})
+					.attr('xlink:href', function(d) {
+						return url(x, y, z);
+					})
+					.attr('x', (x + tx) * k)
+					.attr('y', (y + ty) * k)
+					.attr('width', k)
+					.attr('height', k)
+					.style('opacity', 0.8);
+			});
+			console.log(d3tile);
 			Object.keys(groups).forEach((groupName) => {
 				const group = groups[groupName];
-				const xy = projection([ group.x, group.y ]);
-				svg
-					.append('circle')
-					.style('fill', 'steelblue')
-					.style('opacity', 0.4)
-					.attr('r', 3 + group.persons.length * 3)
-					.attr('cx', xy[0])
-					.attr('cy', xy[1]);
+				const [ x, y ] = projection([ group.x, group.y ]);
+
+				const liner = d3.line().curve(d3.curveBasis).x((d) => d[0]).y((d) => d[1]);
+
+				if (x > 0 && x < width && y > 0 && y < height) {
+					svg
+						.append('circle')
+						.style('fill', 'orange')
+						.style('mix-blend-mode', 'multiply')
+						.style('opacity', 0.7)
+						.attr('r', 3 + group.persons.length * 2)
+						.attr('stroke-width', 2)
+						.attr('stroke', 'black')
+						.attr('cx', x)
+						.attr('cy', y);
+
+					Object.keys(group.edges).forEach((edgeKey) => {
+						const edge = group.edges[edgeKey];
+						const targetX = groups[edgeKey].x;
+						const targetY = groups[edgeKey].y;
+						if (targetX !== group.x) {
+							const [ ex, ey ] = projection([ targetX, targetY ]);
+							if (ex > 0 && ex < width && ey > 0 && ey < height) {
+								const d = liner([ [ x, y ], [ ex, ey + 20 ] ]);
+
+								const edgeW = edge.length - 0.5;
+								if (edgeW) {
+									console.log(edgeW);
+									svg
+										.append('path')
+										.attr('stroke-width', edgeW)
+										.attr('fill', 'none')
+										.attr('stroke', 'black')
+										.attr('d', function(d) {
+											const dx = x - ex;
+											const dy = y - y;
+											const dr = Math.sqrt(dx * dx + dy * dy);
+											return 'M' + x + ',' + y + 'A' + dr + ',' + dr + ' 0 0,1 ' + ex + ',' + ey;
+										});
+								}
+							}
+						}
+					});
+				}
 			});
 		});
 	});
