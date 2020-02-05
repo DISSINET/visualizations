87b54dc6f12ff5fc437c84d9dd69f588366fb2c5
adammmertel <mertel.adam@gmail.com>
2020-02-05 11:54:50 +0100

    refactor: URLs to data in separate file

---
+++ app/urls.ts
@@ -0,0 +1,15 @@
+var urls = {
+	kent: {
+		edges: '1AxApzeVYdh5xEyrYCIMC3ARUbpXIRq2I5mDmNmVzsYY/1',
+		nodes: '1oU4fwqaUgSnbv9NTjAQbSIooF9J5axzt5MfYCPWEhWE/2'
+	},
+	coventry: {
+		nodes: '1FdWb1A7lwW2j1tf9Fswj6C7LxFrb4MXbY7KTHrIjqh8/1',
+		edges: '1ABeHDLXde59akcKwsToldnW04nJU9lpdmt6wPfstnqM/1'
+	},
+	gugliemites: {
+		nodes: '1rIcda6bQeEallBHNzjQvvaedReqtW5DWxoUw30dCecQ/2'
+	}
+};
+
+export default urls;
